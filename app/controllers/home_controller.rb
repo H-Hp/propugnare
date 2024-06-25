8ba7efae157e572204fcfeaf3c7832cc63e31ffe
Hiroki_Macbook <yarebadekirusa3333@yahoo.co.jp>
2024-06-25 16:11:39 +0900

    feat:対戦受付コントローラー/ルーティング/view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,24 @@
 class HomeController < ApplicationController
   def index
+    @battles = Battle.where(status: 'waiting')
   end
+
+  def create
+    @battle = Battle.new(creator_id: 1, status: 'waiting' ,mode: 'typing')
+    if @battle.save
+      redirect_to home_index_path, notice: '対戦予約が作成されました'
+    else
+      render :index
+    end
+  end
+
+  def join
+    @battle = Battle.find(params[:id])
+    if @battle.update(opponent_id: 2, status: 'matched')
+      redirect_to home_index_path, notice: '対戦に参加しました'
+    else
+      redirect_to home_index_path, alert: '対戦への参加に失敗しました'
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  root 'home#index'
-  get '/'  => 'home#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #root 'home#index'
+  root to: "home#index"
+  #get '/'  => 'home#index'
+
+  resources :home, only: [:index, :create] do
+    member do
+      post 'join'
+    end
+  end
 end
